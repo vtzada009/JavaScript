18f767811441e646785dd6c56a232c7b15a9b042
vtzada009 <vitorenzo987@gmail.com>
2026-09-04 17:26:12 -0300

    add declaracao-let.js

--- declaracao-let.js
+++ declaracao-let.js
@@ -1,10 +1,10 @@
 function exemploVar () {
     let x = 10;
     if (true) {
-        let x = 20; // mesma variavel x é redeclaeada dentro do bloco
-        console.log(x); // 20
+        let x = 20; // nova variavel v e criada dentro do bloco
+        console.log(x); // 20 (dentro do bloco)
     }
-    console.log(x); // 20 pois o valor foi alterado 
+    console.log(x); // 10 (fora do bloco)
 }
 
 exemploVar();
