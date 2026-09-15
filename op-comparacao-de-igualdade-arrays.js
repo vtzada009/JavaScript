ad4c421def93086b3f6c298e3a8460afbe27827b
vtzada009 <vitorenzo987@gmail.com>
2026-09-15 16:46:02 -0300

    operador logico conjunção

--- op-comparacao-de-igualdade-arrays.js
+++ op-comparacao-de-igualdade-arrays.js
@@ -4,4 +4,4 @@ let listaA = [10, 20, 30, 40, 50];
 //listaB é outro array com os mesmo 5 elementos que listaA
 let listaB = [10, 20, 30, 40, 50];
 
-console.log(listaA == listaB);
+console.log(listaA == listaB); 
